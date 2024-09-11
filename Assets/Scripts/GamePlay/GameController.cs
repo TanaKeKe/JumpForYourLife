f9ea90d50d98297e08001ed0cff5f4056f095652
TanaKeKe <phanthanhtan2004@gmail.com>
2024-09-12 02:53:43 +0700

    add: CameraController

---
+++ Assets/Scripts/GamePlay/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameController : Singleton<GameController>
+{
+    [SerializeField] private Camera myCamera;
+    [SerializeField] private GameObject bars;
+    [SerializeField] private float rangeCamera;
+    [SerializeField] private float countChangeCamera;
+    [Space(10)]
+    private Vector3 _targetPosition;
+    public void ChangePositionCamera(float distanceJump)
+    {
+        Debug.Log("Thay đổi camera" + distanceJump);
+        _targetPosition = myCamera.transform.position + Vector3.down * (distanceJump);
+        StartCoroutine(CoroutineSmooth());
+        
+    }
+
+    private IEnumerator CoroutineSmooth()
+    {
+        while (myCamera.transform.position.y > _targetPosition.y)
+        {
+            myCamera.transform.position += Vector3.down * countChangeCamera;
+            if (myCamera.transform.position.y == _targetPosition.y) break;
+            yield return null;
+        }
+    }
+
+    
+    public void ChangePositionBars(float distanceJump)
+    {
+        Debug.Log("Thay đổi vị trí của 2 thanh tắt trigger" + bars.transform.position);
+        bars.transform.position += (Vector3.down * distanceJump);
+    }
+
+    public float GetRangeTopCamera()
+    {
+        return myCamera.transform.position.y + rangeCamera;
+    }
+
+    public float GetRangeBottomCamera()
+    {
+        return myCamera.transform.position.y - rangeCamera;
+    }
+}
