56035d062f49072bde7ab2b83971f1bbf76d5dbe
TanaKeKe <phanthanhtan2004@gmail.com>
2024-09-12 02:54:03 +0700

    add: PlayerJump

--- Assets/Scripts/GamePlay/Player.cs
+++ Assets/Scripts/GamePlay/Player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
@@ -6,9 +7,11 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D _rigidbody2d;
     private Collider2D _collider2D;
-
     private bool _isJump;
-
+    private float _positionStartJump;
+    private float _positionEndJump;
+    private float _distanceJump;
+    private bool _startGame;
     private void Awake()
     {
         _rigidbody2d = GetComponent<Rigidbody2D>();
@@ -19,11 +22,15 @@ public class Player : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && _isJump == false)
         {
+            
+            _startGame = true;
             Debug.Log("Ấn vào màn hình");
             Messenger.Broadcast(EventKey.PlayerJump); // Phát sự kiện JUMP
             _isJump = true;
             if (_isJump)
             {
+                _positionStartJump = transform.position.y;
+                transform.SetParent(null);
                 _rigidbody2d.AddForce(Vector2.up * force);
             }
         }
@@ -33,16 +40,29 @@ public class Player : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Block"))
         {
-            // transform.SetParent(null);
             Debug.Log("BLock va chạm nhân vật nè");
             setConnectBlock(collision);
+            SetDistanceJump();
+            if (_startGame)
+            {
+                GameController.Instance.ChangePositionCamera(_distanceJump);
+                GameController.Instance.ChangePositionBars(_distanceJump);
+            }
         }
     }
+    
 
     private void setConnectBlock(Collision2D collision)
     {
         _isJump = false;
         // collion là khối player đang va chạm => cho khối player đang va chạm làm bố của nhân vật
         transform.SetParent(collision.transform);
+    }
+
+    public void SetDistanceJump()
+    {
+        _positionEndJump = transform.position.y;
+        _distanceJump = Math.Abs(_positionEndJump - _positionStartJump);
+        _distanceJump = (float)Math.Round(_distanceJump,1);
     }
 }
