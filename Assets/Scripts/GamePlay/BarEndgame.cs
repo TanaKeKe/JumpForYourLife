5168e6f6597d2c6c0b44831c43a7cb4e36c3e182
TanaKeKe <phanthanhtan2004@gmail.com>
2024-09-12 13:54:14 +0700

    add: Way to Endgame

---
+++ Assets/Scripts/GamePlay/BarEndgame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarEndgame : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("OnExitTrigger2D: Nhân vật chạm vào thanh endgame");
+            Time.timeScale = 0;
+            GameController.Instance._isFinish = true;
+        }
+    }
+}
